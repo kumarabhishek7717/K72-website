ed6cd821d4e4ec384e6ee2e055b901c4d40cc5f0
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2026-01-08 11:19:55 +0530

    done

--- k72 yt/src/App.jsx
+++ k72 yt/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Agence  from './pages/Agence'
 import Project from './pages/Project'
 import Navbar from './components/Navigation/Navbar'
+import FullScreenNav from './components/Navigation/FullScreenNav'
 
 const App = () => {
 
@@ -12,12 +13,13 @@ const App = () => {
 
   return (
     <div >
-      <Navbar />
-      <Routes>
+     { /* <Navbar /> */}
+      <FullScreenNav />
+    {/*   <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/agence' element={<Agence/>}/>
         <Route path='/project' element={<Project/>}/>
-      </Routes>
+      </Routes>*/}
     </div>
   )
 }
--- k72 yt/src/components/Navigation/FullScreenNav.jsx
+++ k72 yt/src/components/Navigation/FullScreenNav.jsx
@@ -2,15 +2,25 @@ import React from 'react'
 
 const FullScreenNav = () => {
   return (
-    <div>
-      <div>
-        <div>
-            <h1>
-                
+    <div id='fullscreennav' className=' text-white h-screen py-40 w-full absolute bg-amber-900'>
+      <div className=''>
+        <div className='link border-t-1 border-white'>
+            <h1 className='font-[font2] text-[8vw] text-center leading-[0.8] pt-10 uppercase'>
+              Projects  
             </h1>
+          <div className='absolute flex '>
+             <div className='flex overflow-auto items-center'>
+              <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-10 uppercase'>Pour Tout voir </h2>
+              <img className='h-36 rounded-full shrink-0 w-96 object-cover ' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt="" />
+               <h2 className='whitespace-nowrap font-[font2] text-[8vw] text-center leading-[0.8] pt-10 uppercase'>Pour Tout voir </h2>
+              <img className='h-36 rounded-full shrink-0 w-96 object-cover ' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt="" />
+
+            </div>
+            </div>
+           </div>
         </div>
       </div>
-    </div>
+    
   )
 }
 
