7e614add04e7977eaa04bfe1b7a5525b6f5850ca
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2026-01-10 14:16:41 +0530

    done

--- k72 yt/src/components/common/Stairs.jsx
+++ k72 yt/src/components/common/Stairs.jsx
@@ -13,49 +13,6 @@ const Stairs = (props) => {
   const pageRef = useRef(null)
 
 
-  useGSAP(function(){
-
-    const tl = gsap.timeline()
-
-     tl.to(stairParentRef.current,{
-      display: 'block',
-      
-    })
-
-
-
-    tl.from('.stair',{
-      height:0,
-      
-      stagger:{
-        amount:-0.25
-      }
-
-    })
-    tl.to('.stair',{
-      y:'100%',
-      stagger:{
-        amount:-0.25
-      }
-
-    })
-    tl.to(stairParentRef.current,{
-      display: 'none'
-    })
-
-     tl.to('.stair',{
-      y:'0%',  
-
-  })
-
-  gsap.from(pageRef.current,{
-    opacity:0,
-    delay:1.3,
-    scale:1.2
-  })
-
-},[currentPath])
-
 console.log(props.children);
 
 
@@ -64,13 +21,7 @@ console.log(props.children);
         
         <div ref = {stairParentRef}className='h-screen w-full  fixed z-10 top-0'>
        
-      <div className='h-full w-full flex fixed '>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-        <div className='stair h-full w-1/5 bg-black'></div>
-      </div>
+      
      </div>
       <div ref={pageRef}>
          {props.children}
--- k72 yt/src/main.jsx
+++ k72 yt/src/main.jsx
@@ -4,12 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Stairs from './components/common/stairs.jsx'
+import NavContext from './context/NavContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <BrowserRouter>
   <Stairs>
- <App />
+  <NavContext>
+     <App />
+  </NavContext>
+
   </Stairs>
 
    </BrowserRouter>
